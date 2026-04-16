b50e72d848f72d0c90d361ace8060f9b3e2cd817
湫曗 <18817832003@qq.com>
2026-04-16 16:18:59 +0800

    chore: add more sample

--- src/dataset.py
+++ src/dataset.py
@@ -9,21 +9,27 @@ endToken = torch.tensor([endTokenId])
 
 class NanoDataSet(torch.utils.data.Dataset):
     def genSamples(self, text: str):
-        samples = []
+        samples:list = []
         for i in range(1, len(text)):
-            x = torch.cat([startToken, torch.tensor([ord(c) for c in text[:i]])])
+            x = torch.cat([startToken, torch.tensor([ord(c)
+                          for c in text[:i]])])
             y = torch.tensor([ord(c) for c in text[: i + 1]])
             samples.append((x, y))
 
         x = torch.cat([startToken, torch.tensor([ord(c) for c in text])])
         y = torch.cat([torch.tensor([ord(c) for c in text]), endToken])
-        
+
         samples.append((x, y))
         return samples
 
     def __init__(self):
         super().__init__()
-        self.samples = self.genSamples("hello world!")
+        self.samples=[]
+        raw_samples = ["hello world!","I'm a bot!", "Good night!"]
+        for sample in raw_samples:
+            samples = self.genSamples(sample)
+            for s in samples:
+                self.samples.append(s)
 
     def __len__(self):
         return len(self.samples)
